f928290acc51ca7a0ade98dc1d3ad08b9fbbf137
Usuario <Usuario@10.22.148.87>
2019-11-13 13:34:20 +0000

    Now uses Scanner in MailListReader.read()

---
+++ src/kata5/DataBase.java
@@ -0,0 +1,54 @@
+package kata5;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DataBase {
+    
+    private final String URL;
+    private Connection connection = null;
+
+    public DataBase(String URL) {
+        this.URL = URL;
+    }
+
+    public void open() {
+        try {
+            this.connection = DriverManager.getConnection(this.URL);
+            System.out.println("Base de datos abierta");
+        } catch (SQLException ex) {
+            System.out.println("ERROR DATABASE::open (SQLException) " + ex.getMessage());
+        }
+    }
+
+    public void close() {
+        try {
+            if(this.connection != null){
+                connection.close();
+                System.out.println("Base de datos cerrada");
+            }
+        } catch (SQLException ex) {
+            System.out.println("ERROR DATABASE::close (SQLException) " + ex.getMessage());
+        }
+    }
+
+    void selectPEOPLE() {
+        String SQL = "SELECT * FROM PEOPLE";
+        try {
+            Statement statement = this.connection.createStatement();
+            ResultSet resultset = statement.executeQuery(SQL);
+            System.out.println("ID\tNAME\tSURNAME\tDEPARTMENT");
+            while(resultset.next()){
+                System.out.println(resultset.getInt("ID") + "\t" + 
+                        resultset.getString("NAME") + "\t" + 
+                        resultset.getString("SURNAME") + "\t" + 
+                        resultset.getString("DEPARTMENT"));
+            }
+            
+        } catch (Exception e) {
+        }
+    }   
+}
